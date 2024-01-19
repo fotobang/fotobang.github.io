2f7abafb076e58b0c7cc68bd456e598fc6006b4e
tomz <tomz>
2024-01-19 22:07:37 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="Brand">
+                <span>AVBOBO</span>
+                <span className="Flag">破解版</span>
+            </div>
+            <div className="Field">
+                <a href="https://faka.tyteam.cn/details/5F15AA3F" target="_blank" className="Button">点击前往购买</a>
+                <h4 className='Title'>购买破解账号，懂的都懂</h4>
+                <ul className="List">
+                    <li>只需35即可享受官方300的永久服务</li>
+                    <li>无限次数观影所有视频</li>
+                    <li>免除观看广告</li>
+                    <li>视频可下载到本地</li>
+                    <li>可极速预览视频</li>
+                    <li>解锁所有楼风信息</li>
+                    <li>账号为永久会员</li>
+                </ul>
+            </div>
+            <div className="Field">
+                <a target="_blank" href="https://bb20.life" className="Title Link">点击打开bobo最新地址登录观看</a>
+            </div>
+            <div className="Note">记得收藏本站，未来还有更多的福利!!!</div>
+        </div>
+    );
+}
+
+export default App;
